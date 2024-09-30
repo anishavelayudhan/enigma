f5e7e88af74df739401193609ebb2fcf07372268
Anisha Velayudhan <anisha.velayudhan@gmail.com>
2024-09-30 16:46:25 +0200

    Initial commit

---
+++ src/Machine.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Machine {
+    private Rotors rotors;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter text to encode: ");
+        String message = scanner.nextLine();
+    }
+
+}
---
+++ src/Rotors.java
@@ -0,0 +1,9 @@
+public class Rotors {
+    private static char[][] rotors = {
+            "jgdqoxuscamifrvtpnewkblzyh".toCharArray(),
+            "ntzpsfbokmwrcdjivlaeyuxhgq".toCharArray(),
+            "jviubhtcdyakeqzposgxnrmwfl".toCharArray()
+    };
+
+
+}
