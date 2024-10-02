3ccd5d1ee4b167e9576aead89d01f93bf20d8a48
Anisha Velayudhan <anisha.velayudhan@gmail.com>
2024-10-02 15:20:25 +0200

    Added encode feature

--- src/Machine.java
+++ src/Machine.java
@@ -1,12 +1,33 @@
 import java.util.Scanner;
 
 public class Machine {
-    private Rotors rotors;
+    private static Rotors rotors;
+
+    public static String cypher(String s) {
+        char[] message = s.toUpperCase().toCharArray();
+        char[] outputMessage = new char[message.length];
+
+        for (int i = 0; i < message.length; i++) {
+            outputMessage[i] = rotors.encodeForward(message[i]);
+            // reflector
+            // outputMessage[i] = rotors.encodeBackward(outputMessage[i]);
+            rotors.rotate();
+
+        }
+
+        rotors.rotate();
+        return new String(outputMessage);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        rotors = new Rotors();
 
-        System.out.println("Enter text to encode: ");
-        String message = scanner.nextLine();
+        System.out.println("\nWelcome to the ENIGMA MACHINE\n");
+        System.out.println("Enter a message to cypher: ");
+        String s = scanner.nextLine();
+
+        System.out.println("\nHere's your encoded/decoded message:\n" + cypher(s) + "\n");
     }
 
 }
--- src/Rotors.java
+++ src/Rotors.java
@@ -1,9 +1,44 @@
 public class Rotors {
     private static char[][] rotors = {
-            "jgdqoxuscamifrvtpnewkblzyh".toCharArray(),
-            "ntzpsfbokmwrcdjivlaeyuxhgq".toCharArray(),
-            "jviubhtcdyakeqzposgxnrmwfl".toCharArray()
+            "JGDQOXUSCAMIFRVTPLNEWKBZYH".toCharArray(),
+            "NTZPSFBOKMWRCDJIVLAEYUXHGQ".toCharArray(),
+            "JVIUBHTCDYAKEQZPOSGXNRMWFL".toCharArray()
     };
 
+    private int[] positions = new int[3];
+
+    public void rotate() {
+        positions[0] = (positions[0] + 1) % 26;
+        if (positions[0] == 0) {
+            positions[1] = (positions[1] + 1) % 26;
+            if (positions[1] == 0) {
+                positions[2] = (positions[2] + 1) % 26;
+            }
+        }
+    }
+
+    public char encodeForward(char c) {
+        char encodedLetter = c;
+        int index = encodedLetter - 'A';
+
+        for (int rotor = 0; rotor < rotors.length; rotor++) {
+            index = (index + positions[rotor]) % 26;
+            encodedLetter = rotors[rotor][index];
+            index = encodedLetter - 'A';
+        }
+        return encodedLetter;
+    }
+
+    public char encodeBackward(char c) {
+        char encodedLetter = c;
+        int index = encodedLetter - 'A';
+
+        // for (int rotor = rotors.length; rotor < rotors.length; rotor--) {
+        //     index = (index + positions[rotor]) % 26;
+        //     encodedLetter = rotors[rotor][index];
+        //     index = encodedLetter - 'A';
+        // }
+        return encodedLetter;
+    }
 
 }
